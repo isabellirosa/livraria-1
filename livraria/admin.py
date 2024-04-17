6887049997fe791c9ecb40fd7fabc1e74e6269e2
isabellirosa <isabelli.ifc@gmail.com>
2024-04-16 21:02:28 -0300

    aulas 5 a 14

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from livraria.models import Autor, Categoria, Editora, Livro
+
+admin.site.register(Autor)
+admin.site.register(Categoria)
+admin.site.register(Editora)
+admin.site.register(Livro)

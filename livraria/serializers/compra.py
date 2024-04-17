a9c470470bcc985a124c5611b2965085294ae900
isabellirosa <isabelli.ifc@gmail.com>
2024-04-17 15:50:44 -0300

    27 - pt 2

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -6,3 +6,7 @@ class CompraSerializer(ModelSerializer):
     class Meta:
         model = Compra
         fields = "__all__"
+        usuario = CharField(source="usuario.email", read_only=True)
+        status = CharField(source="get_status_display", read_only=True)
+
+    

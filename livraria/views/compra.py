353e46a36a170378e0707586910ead0e26a1d8f8
isabellirosa <isabelli.ifc@gmail.com>
2024-04-18 08:40:23 -0300

    aula 33

--- livraria/views/compra.py
+++ livraria/views/compra.py
@@ -4,12 +4,13 @@ from livraria.models import Compra
 from livraria.serializers import CompraSerializer, CriarEditarCompraSerializer
 
 
-
 class CompraViewSet(ModelViewSet):
     queryset = Compra.objects.all()
-    serializer_class = CompraSerializer
 
-    def get_serializer_class(self):
-        if self.action == "create" or self.action == "update":
-            return CriarEditarCompraSerializer
-        return CompraSerializer    
+    def get_queryset(self):
+        usuario = self.request.user
+        if usuario.is_superuser:
+            return Compra.objects.all()
+        if usuario.groups.filter(name="Administradores"):
+            return Compra.objects.all()
+        return Compra.objects.filter(usuario=usuario)      
